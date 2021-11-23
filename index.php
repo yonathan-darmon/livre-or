1da22160b5d8f5f473bb219d7731a8284f193e72
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-23 16:35:42 +0100

    travail sur l index

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start()
 ?>
 <!doctype html>
 <html lang="en">
@@ -7,6 +8,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="asset/css/index.css">
+
     <title>accueil</title>
 </head>
 <body>
@@ -14,6 +17,11 @@
 
 </header>
 <main>
+    <video autoplay muted loop id="myVideo">
+        <source src="asset/video/espace.mov" type="video/mp4">
+    </video>
+    <h1 class="alien">Bienvenue sur le livre d'or de la Terre</h1>
+    <h1 class="titre"><span>B</span><span>i</span><span>e</span><span>n</span><span>v</span><span>e</span><span>n</span><span>u</span><span>e</span> <span>s</span><span>u</span><span>r</span> <span>l</span><span>e</span> <span>l</span><span>i</span><span>v</span><span>r</span><span>e </span><span>d</span><span>'</span><span>o</span><span>r</span> <span>d</span><span>e</span> <span>l</span><span>a</span> <span>T</span><span>e</span><span>r</span><span>r</span><span>e</span></h1>
 
 </main>
 <footer>
