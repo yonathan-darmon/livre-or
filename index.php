7b97c3cc085ad98ff9b8586872a4894ae853f7db
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-23 17:21:14 +0100

    fin de la page Index

--- index.php
+++ index.php
@@ -20,8 +20,12 @@ session_start()
     <video autoplay muted loop id="myVideo">
         <source src="asset/video/espace.mov" type="video/mp4">
     </video>
-    <h1 class="alien">Bienvenue sur le livre d'or de la Terre</h1>
-    <h1 class="titre"><span>B</span><span>i</span><span>e</span><span>n</span><span>v</span><span>e</span><span>n</span><span>u</span><span>e</span> <span>s</span><span>u</span><span>r</span> <span>l</span><span>e</span> <span>l</span><span>i</span><span>v</span><span>r</span><span>e </span><span>d</span><span>'</span><span>o</span><span>r</span> <span>d</span><span>e</span> <span>l</span><span>a</span> <span>T</span><span>e</span><span>r</span><span>r</span><span>e</span></h1>
+    <h1 class="titre"><span>B</span><span>i</span><span>e</span><span>n</span><span>v</span><span>e</span><span>n</span><span>u</span><span>e</span>
+        <span>s</span><span>u</span><span>r</span> <span>l</span><span>e</span>
+        <span>l</span><span>i</span><span>v</span><span>r</span><span>e </span><span>d</span><span>'</span><span>o</span><span>r</span>
+        <span>d</span><span>e</span> <span>l</span><span>a</span>
+        <span>T</span><span>e</span><span>r</span><span>r</span><span>e</span></h1>
+    <a href="#" class="enter">Bienvenue</a>
 
 </main>
 <footer>
